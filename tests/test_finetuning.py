5238b467800c622cd71e3adf6ae877810ab75b67
Nathan Klapstein <nklapste@ualberta.ca>
2020-01-10 12:42:27 -0700

    fixed incorrect param name

--- tests/test_finetuning.py
+++ tests/test_finetuning.py
@@ -215,7 +215,7 @@ def test_score_emoji():
     # Find top emojis for each sentence
     for i, t_prob in enumerate(prob):
         assert np.array_equal(
-            top_elements(t_prob, 5), TEST_SENTENCES_EXPECTED_EMOJI_INDEXES[i]
+            top_elements(t_prob, 5), expected[i]
         )
 
 
