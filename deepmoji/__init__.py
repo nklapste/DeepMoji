96ce4aeaf539fe5c2dfb9aecdde95ca896bc7560
Nathan Klapstein <nklapste@ualberta.ca>
2020-01-10 12:32:40 -0700

    adding docs and version info in `deepmoji/__ini__.py`

---
+++ deepmoji/__init__.py
@@ -0,0 +1,9 @@
+"""DeepMoji
+
+DeepMoji is a model trained on 1.2 billion tweets with emojis to
+understand how language is used to express emotions. Through transfer learning the
+model can obtain state-of-the-art performance on many emotion-related text modeling
+tasks.
+"""
+
+__version__ = (1, 0, 0)  # TODO: bump
