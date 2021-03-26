abb274a42bce8f38d57ce42425df029e9a15ac74
Nathan Klapstein <nklapste@ualberta.ca>
2021-03-26 19:19:38 -0400

    renaming field `trainable_weights` to `_trainable_weights`

--- deepmoji/attlayer.py
+++ deepmoji/attlayer.py
@@ -31,7 +31,7 @@ class AttentionWeightedAverage(Layer):
         self.W = self.add_weight(shape=(input_shape[2], 1),
                                  name='{}_W'.format(self.name),
                                  initializer=self.init)
-        self.trainable_weights = [self.W]
+        self._trainable_weights = [self.W]
         super(AttentionWeightedAverage, self).build(input_shape)
 
     def call(self, x, mask=None):
